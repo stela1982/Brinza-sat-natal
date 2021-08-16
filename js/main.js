eed9ab1a4e4766b4a9dce0811c535414c5d0b3be
stela1982 <89011831+stela1982@users.noreply.github.com>
2021-08-16 12:21:19 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function salutare(){
+  alert("Salut");
+    prompt("Faceți o vizită imaginară")
+}
+
+function show(){
+    document.getElementById("side-menu").classList.toggle("show");
+}
+function data() {
+    
+    var text;
+
+    
+    text = nume+prenume;
+    
+   alert(text);
+    document.getElementById("text-data").innerHTML=text;
+}
+
